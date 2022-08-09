facf358a8d1ab9128e096a0dd0ec9d736ff9e185
Windows 10 <omid.mohamadyari1986@gmail.com>
2022-08-09 05:01:39 -0700

    -firstCommit

---
+++ cypress/support/pages/loginError.js
@@ -0,0 +1,3 @@
+const loginEmailError = "//label[@id='loginEmail-error']"
+const forgot = "//*[text()='Forgot Password?']"
+const loginPasswordError = "//*[text()='Please use a password of at least 8 characters with at least one lowercase letter, one uppercase letter, one number and one special character.']"
