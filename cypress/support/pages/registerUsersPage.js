3be534ae1ff442c2b3fa7a37b24d1fc6dde06749
Windows 10 <omid.mohamadyari1986@gmail.com>
2022-08-11 10:19:57 -0700

    -New commit for Kamali

--- cypress/support/pages/registerUsersPage.js
+++ cypress/support/pages/registerUsersPage.js
@@ -1,65 +1,65 @@
-// class registerUserPage{
-//
-//     elements = {
-//         salutation:()=>cy.get('//select[@id="salutation"]'),
-//         firstNameInput:()=>cy.get('//input[@id="firstName"]'),
-//         lastNameInput:()=>cy.get('//input[@id="lastName"]'),
-//         emailNewAccountInput:()=>cy.get('.accountNew > .formInput.formInput--text > .formInput__inputContainer > #email'),
-//         passwordInput:()=>cy.get('//input[@id="password"]'),
-//         passwordRepInput:()=>cy.get('//input[@id="password2"]'),
-//         checkboxNewsletter:()=>cy.get('.accountNew__newsletterCheckbox >.accountNew__checkbox >.checkbox.checkbox--alignTop'),
-//         checkBoxRuleApprove:()=>cy.get('.accountNew__agbCheckbox > .accountNew__checkbox > .checkbox.checkbox--alignTop'),
-//         buttonSubmit:()=>cy.get('//button[@id="register-submit"]'),
-//         emailDownInput:()=>cy.get('//input[@id="email"and@class="input input----text"]'),
-//         firstNameInputerror:()=>cy.get('//span[@id="firstName-error"]'),
-//         lastNameInputerror:()=>cy.get('//span[@id="lastName-error"]'),
-//         passwordRepInputError:()=>cy.get('//span[@id="password2-error"]'),
-//         checkBoxRuleApproveError:()=>cy.get('//span[@id="accountNew__agbBoxErrorId"]'),
-//
-//
-//     }
-//
-//
-//     typeFirstName(firstname)
-//     {
-//         this.elements.firstNameInput().type(firstname);
-//     }
-//
-//     typeLastName(lastname)
-//     {
-//         this.elements.lastNameInput().type(lastname);
-//     }
-//
-//     typeEmail(email)
-//     {
-//         this.elements.emailNewAccountInput().type(email);
-//     }
-//
-//     typePassword(password)
-//     {
-//         this.elements.passwordInput().type(password);
-//     }
-//
-//     typePasswordRep(passwordrep)
-//     {
-//         this.elements.passwordRepInput().type(passwordrep);
-//     }
-//
-//     clickCheckboxNewsletter()
-//     {
-//         this.elements.checkboxNewsletter().click();
-//     }
-//
-//     clickCheckboxRuleApprove()
-//     {
-//         this.elements.checkBoxRuleApprove().click();
-//     }
-//
-//     clickButtonSubmit()
-//     {
-//         this.elements.buttonSubmit().click();
-//     }
-//
-//
-// }
-// module.exports = new registerUserPage();
+class registerUserPage{
+
+    elements = {
+        salutation:()=>cy.get('//select[@id="salutation"]'),
+        firstNameInput:()=>cy.get('//input[@id="firstName"]'),
+        lastNameInput:()=>cy.get('//input[@id="lastName"]'),
+        emailNewAccountInput:()=>cy.get('.accountNew > .formInput.formInput--text > .formInput__inputContainer > #email'),
+        passwordInput:()=>cy.get('//input[@id="password"]'),
+        passwordRepInput:()=>cy.get('//input[@id="password2"]'),
+        checkboxNewsletter:()=>cy.get('.accountNew__newsletterCheckbox >.accountNew__checkbox >.checkbox.checkbox--alignTop'),
+        checkBoxRuleApprove:()=>cy.get('.accountNew__agbCheckbox > .accountNew__checkbox > .checkbox.checkbox--alignTop'),
+        buttonSubmit:()=>cy.get('//button[@id="register-submit"]'),
+        emailDownInput:()=>cy.get('//input[@id="email"and@class="input input----text"]'),
+        firstNameInputerror:()=>cy.get('//span[@id="firstName-error"]'),
+        lastNameInputerror:()=>cy.get('//span[@id="lastName-error"]'),
+        passwordRepInputError:()=>cy.get('//span[@id="password2-error"]'),
+        checkBoxRuleApproveError:()=>cy.get('//span[@id="accountNew__agbBoxErrorId"]'),
+
+
+    }
+
+
+    typeFirstName(firstname)
+    {
+        this.elements.firstNameInput().type(firstname);
+    }
+
+    typeLastName(lastname)
+    {
+        this.elements.lastNameInput().type(lastname);
+    }
+
+    typeEmail(email)
+    {
+        this.elements.emailNewAccountInput().type(email);
+    }
+
+    typePassword(password)
+    {
+        this.elements.passwordInput().type(password);
+    }
+
+    typePasswordRep(passwordrep)
+    {
+        this.elements.passwordRepInput().type(passwordrep);
+    }
+
+    clickCheckboxNewsletter()
+    {
+        this.elements.checkboxNewsletter().click();
+    }
+
+    clickCheckboxRuleApprove()
+    {
+        this.elements.checkBoxRuleApprove().click();
+    }
+
+    clickButtonSubmit()
+    {
+        this.elements.buttonSubmit().click();
+    }
+
+
+}
+module.exports = new registerUserPage();
