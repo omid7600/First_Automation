00b795d58929d12d842062b578a11e2f5f1378d7
omid7600 <omid_mohamadyari@yahoo.com>
2022-10-28 14:36:02 +0330

    committ - Kamali

--- cypress/pages/homePage.js
+++ cypress/pages/homePage.js
@@ -1,11 +1,55 @@
+require('cypress-xpath');
+
 class homePage {
 
     elements = {
-        loginUserElement: () => cy.get('.headerElement__status.headerElement__status--login'),
+
+		furniture:()=>cy.xpath('//a[@class="menu__linkHref menu__linkHref--default"and@href="/moebel"]'),
+		Christmas:()=>cy.xpath('//a[@class="menu__linkHref menu__linkHref--default"and@href="/weihnachten"]'),
+		kitchens:()=>cy.xpath('//a[@class="menu__linkHref menu__linkHref--default"and@href="/kuechenstudio"]'),
+		household:()=>cy.xpath('//a[@class="menu__linkHref menu__linkHref--default"and@href="/haushalt"]'),
+		bedding:()=>cy.xpath('//a[@class="menu__linkHref menu__linkHref--default"and@href="/bettwaren"]'),
+		mattresses:()=>cy.xpath('//a[@class="menu__linkHref menu__linkHref--default"and@href="/matratzen"]'),
+		carpets:()=>cy.xpath('//a[@class="menu__linkHref menu__linkHref--default"and@href="/teppiche"]'),
+		textiles:()=>cy.xpath('//a[@class="menu__linkHref menu__linkHref--default"and@href="/textilien"]'),
+		to_shine:()=>cy.xpath('//a[@class="menu__linkHref menu__linkHref--default"and@href="/lampen"]'),
+		accessories:()=>cy.xpath('//a[@class="menu__linkHref menu__linkHref--default"and@href="/accessoires"]'),
+		children:()=>cy.xpath('//a[@class="menu__linkHref menu__linkHref--default"and@href="/baby-kind"]'),
+		Brands:()=>cy.xpath('//a[@class="menu__linkHref menu__linkHref--default"and@href="/marken-serien"]'),
+		Sale:()=>cy.xpath('//a[@class="menu__linkHref menu__linkHref--default"and@href="/angebote"]'),
+		inspiration:()=>cy.xpath('//a[@class="menu__linkHref menu__linkHref--default"and@href="/inspiration"]'),
+		wishListHome:()=>cy.xpath('//a[@class="headerElement__link headerElement__link--wishlist"and@href="/wunschliste"]'),
+		shoppingCartHome:()=>cy.xpath('//a[@class="headerElement__link headerElement__link--cart headerElement__link--cartFilled"and@href="/warenkorb"],'),
+		couches:()=>cy.xpath('//a[text()="Sofas"and@href="/sofas"]'),
+		cornerSofas:()=>cy.xpath('//a[@class="sidebarNavigation__thirdLevelCategoryName"and@href="/schlafsofas"]'),
+		product001:()=>cy.xpath('//div[@class="wishlistIcon wishlistIcon--pointer"and@data-wish-list-entry-number="23403766"]'),
+		product002:()=>cy.xpath('//div[@class="wishlistIcon wishlistIcon--pointer"and@data-wish-list-entry-number="20403530"]'),
+		product003:()=>cy.xpath('//div[@class="wishlistIcon wishlistIcon--pointer"and@data-wish-list-entry-number="25405544"]'),
+
     }
 
-    getUserHeader(){
-        this.elements.loginUserElement()
-    }
+	clickFurniture() {
+		this.elements.furniture().click();
+	}
+	clickCouches() {
+		this.elements.couches().click();
+	}
+	clickCornerSofas() {
+		this.elements.cornerSofas().click();
+	}
+	clickProduct001() {
+		this.elements.product001().click();
+	}
+	clickProduct002() {
+		this.elements.product002().click();
+	}
+	clickProduct003() {
+		this.elements.product003().click();
+	}
+	clickWishListHome() {
+		this.elements.wishListHome().click();
+	}
+
 }
+
 module.exports = new homePage();
